22466fddf47c25f5d64f3402628e9555aa169c21
Karen Novaes <karen-nd@outlook.com>
2022-06-23 09:25:07 -0300

    adicionado mais palavras reconhecidas

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const tmi = require('tmi.js');
-
 const client = new tmi.Client({
     options: { debug: true },
     identity: {
         username: 'inspetordegaita',
-        password: process.env.token
+        password: process.env.token,
     },
     channels: ['karennovaesd']
 });
-
 client.connect();
-
 client.on('message', (channel, tags, message, self) => {
     var mensagem = new Array();
     mensagem = message.toLowerCase().split(" ");
     if (mensagem.includes("sanfona")) {
-        client.action("karennovaesd", tags['display-name'] + " Usou a palavra proibida! É GAITAAAAAA");
+        client.action("karennovaesd", `${tags['display-name']} Usou uma palavra proibida! É GAITAAAAAA`);
     }
-
+    if (mensagem.includes("mandioca") || mensagem.includes("mmacaxeira")) {
+        client.action("karennovaesd", `${tags['display-name']} Usou uma palavra proibida! É AIPIIIIM`);
+    }
+    if (mensagem.includes("tangerina")) {
+        client.action("karennovaesd", `${tags['display-name']} Usou uma palavra proibida! É BERGAMOTAAAA`);
+    }
 });
